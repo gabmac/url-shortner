aa7e4f347e5ca2057c84968c9714adaff9efb088
Gabriel <gabrielbonanimachado@gmail.com>
2023-08-20 11:43:18 -0300

    feat: Response Schema Description

--- system/application/dto/api/response/url_response.py
+++ system/application/dto/api/response/url_response.py
@@ -1,16 +1,23 @@
 from datetime import datetime
+from typing import List
+
+from pydantic import Field
 
 from system.application.dto.base_dto import BaseDTO
 from system.domain.enums.short_url_enum import ShortUrlStatusEnum
 
 
 class ShortUrlResponse(BaseDTO):
-    target_url: str
-    short_url: str
-    created_at: datetime
-    updated_at: datetime
-    status: ShortUrlStatusEnum
+    target_url: str = Field(description="Destination URL")
+    short_url: str = Field(description="Short URL")
+    created_at: datetime = Field(description="Created time in UTC")
+    updated_at: datetime = Field(description="Last Update Time in UTC")
+    status: ShortUrlStatusEnum = Field(description="Possible Short Url Status")
 
 
 class ViewShortUrlResponse(BaseDTO):
     response: ShortUrlResponse
+
+
+class ListAllShortUrlResponse(BaseDTO):
+    response = List[ShortUrlResponse]
